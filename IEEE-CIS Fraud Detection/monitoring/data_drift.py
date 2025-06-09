0af1da9bd9497edb4e4453bb0c79a9c9024ae1cc
udomercy <udomercyokon11@gmail.com>
2025-06-09 16:14:00 +0000

    enabled Feature-Type-Aware Drift Detection - fixes

--- IEEE-CIS Fraud Detection/monitoring/data_drift.py
+++ IEEE-CIS Fraud Detection/monitoring/data_drift.py
@@ -1,5 +1,15 @@
-from scipy.stats import ks_2samp
+from scipy.stats import ks_2samp, chi2_contingency
+import numpy as np
 
-def detect_drift(train_col, new_col):
-    stat, p_value = ks_2samp(train_col, new_col)
-    return p_value < 0.05
+def numeric_drift(train, live):
+    stat, p = ks_2samp(train, live)
+    return p < 0.05
+
+def categorical_drift(train, live):
+    table = np.vstack([
+        train.value_counts(),
+        live.value_counts()
+    ]).fillna(0)
+
+    _, p, _, _ = chi2_contingency(table)
+    return p < 0.05
